3af56344e10f39d8df83e57fe47e2fe6f5ab7605
Rossana <rossanaSendoya@gmail.com>
2026-04-10 17:23:36 -0400

    Añadir clases

---
+++ CapaEntidad/ECategoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class ECategoria
+    {
+        public int IdCategoria { get; set; }
+        public int NOmbreCatego { get; set; }
+    }
+}
---
+++ CapaEntidad/EProducto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class EProducto
+    {
+        public int IdProducto { get; set; }
+        public int IdCategoria { get; set; }
+        public string NombreProducto { get; set; }
+        public decimal PrecioCon { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public bool Estado { get; set; }
+        public string FechaRegistro { get; set; }
+
+    }
+}
